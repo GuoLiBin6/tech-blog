12d5a703046c2a4a3eb5482d9f1f40099883b9ff
GuoLiBin6 <glbin533@163.com>
2022-06-26 12:15:08 +0800

    feat: 增加介绍及备案信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,26 @@
+const padLeft0 = (num) => {
+    return num > 9 ? num : `0${num}`
+}
+
+const timeFormatter = (time) => {
+    const date = new Date(time)
+    return `${date.getFullYear()}-${padLeft0(date.getMonth() + 1)}-${padLeft0(date.getDate())} ${padLeft0(date.getHours())}:${padLeft0(date.getMinutes())}:${padLeft0(date.getSeconds())}`
+}
+
 module.exports = {
     base: '/tech-blog/',
     title: "前端知识体系",
     description: "收录前端学习涉及的内容总结",
     plugins: [
-        "vuepress-plugin-cat"
+        "vuepress-plugin-cat",
+        [
+            '@vuepress/last-updated', // "上次更新"时间格式
+            {
+                transformer: (timestamp, lang) => {
+                    return timeFormatter(timestamp)
+                },
+            },
+        ],
     ],
     theme: 'vdoing',
     themeConfig: {
@@ -13,6 +30,14 @@ module.exports = {
             avatar: '/tech-blog/images/avatar.jpeg',
             name: 'GuoLiBin6',
             slogan: '程序员永不下班',
+        },
+        repo: 'GuoLiBin6/tech-blog',
+        lastUpdated: '上次更新',
+         // 页脚信息
+        footer: {
+            createYear: 2022, // 博客创建年份
+            copyrightInfo:
+            'GuoLiBin6 </br>冀ICP备2022013865号-1', // 博客版权信息，支持a标签或换行标签</br>
         },
     },
     // 监听文件变化并重新构建
