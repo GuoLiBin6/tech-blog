1edddc048afd603c96cf0aa8d1061d75ddf69947
GuoLiBin6 <glbin533@163.com>
2022-06-29 21:45:35 +0800

    feat: 补充大体目录，收录网站，数据算法-栈

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,6 +21,15 @@ module.exports = {
                 },
             },
         ],
+        [
+            'one-click-copy', // 代码块复制按钮
+            {
+                copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+                copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+                duration: 1000, // prompt message display time.
+                showInMobile: false, // whether to display on the mobile side, default: false.
+            },
+        ],
     ],
     theme: 'vdoing',
     themeConfig: {
