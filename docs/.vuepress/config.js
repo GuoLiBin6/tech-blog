9c4d0653a50f0091add5bb82f4ed60b6dbb9dc6d
GuoLiBin6 <glbin533@163.com>
2022-07-01 15:48:52 +0800

    feat: 补充栈的介绍,增加图片缩放插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,6 +30,15 @@ module.exports = {
                 showInMobile: false, // whether to display on the mobile side, default: false.
             },
         ],
+        [
+            'vuepress-plugin-zooming', // 放大图片
+            {
+                selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+                options: {
+                    bgColor: 'rgba(0,0,0,0.6)',
+                },
+            },
+        ],
     ],
     theme: 'vdoing',
     themeConfig: {
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -41,12 +41,10 @@ function integrateGitalk(router) {
         // 如果url路径有中文则使用decodeURIComponent，否则可以直接使用location.pathname
         const path = decodeURIComponent(location.pathname)
         const hList = document.getElementsByTagName('h1')
-        console.log('hList', hList)
         let title = path.split('/').pop() || path
         if (hList[0] && hList[0].innerText) {
             title = `评论 ${hList[0].innerText}`
         }
-        console.log(title)
         const gitalk = new Gitalk({
             clientID: 'a8bd0f836a9a0ea7a5fb',
             clientSecret: 'ba6976ade248e7b6c55ecfcf5d5a1b419553e246',
