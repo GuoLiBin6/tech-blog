29d3441ea9f70800c494ec04edd1078459a39eb3
root <root@glb.office.yunion.io>
2023-04-21 18:31:43 +0800

    feat: 增加 图片组件,精简代码

---
+++ docs/.vuepress/components/g-img.vue
@@ -0,0 +1,31 @@
+<template>
+  <div align="center">
+    <img v-bind="binds" :src="imgUrl">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GImg',
+  props: {
+    src: String,
+    name: String,
+    h: {
+      type: String,
+      default: "250",
+    },
+    w: String,
+  },
+  computed: {
+    binds () {
+      const ret = {}
+      if (this.h) ret.height = this.h
+      if (this.w) ret.width = this.w
+      return ret
+    },
+    imgUrl () {
+      return this.src ? this.src : 'https://gcore.jsdelivr.net/gh/GuoLiBin6/images/tech-blog/' + this.name
+    }
+  }
+}
+</script>
